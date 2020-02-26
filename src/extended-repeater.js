36a0a6c0d08564062674f767bb87a459307e846a
Дмитрий <daniel24111993@mail.ru>
2020-02-26 20:07:39 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|' } = options;
+
+  function getRepeatedArray(value, length) {
+    const repeatedArray = [];
+    for(let i = 1; i <= length; i++) {
+      repeatedArray.push(String(value));
+    }
+
+    return repeatedArray;
+  }
+
+  str += (typeof addition !== 'undefined') ? getRepeatedArray(addition, additionRepeatTimes).join(additionSeparator) : '';
+
+  return getRepeatedArray(str, repeatTimes).join(separator);
 };
-  
